2e8e6b9237dfea62e7a280326ce00446ede9c0ab
Junior Dering <juniordering@hotmail.com>
2023-10-08 17:19:25 -0400

    feat: improving metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,50 @@ import '@/styles/tailwind.css'
 export const metadata: Metadata = {
   title: {
     template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    default: 'Studio - Desbloqueie o potencial da sua presença na web.',
+  },
+  alternates: {
+    canonical: 'https://studio-teal.vercel.app',
+    languages: {
+      pt: 'pt-BR',
+    },
+  },
+  themeColor: 'light',
+  description:
+    'Enquanto outros serviços agrícolas são inovadores, nós abraçamos a tecnologia dos drones para aumentar a eficiência e a precisão.',
+  openGraph: {
+    title: 'Studio',
+    siteName: 'Studio',
+    locale: 'pt_BR',
+    images: [
+      {
+        url: 'favicon.ico',
+        width: 629,
+        height: 629,
+        alt: 'Um D grande seguindo de enuv em minusculo',
+      },
+      {
+        url: 'favicon.ico' as string,
+        width: 1800,
+        height: 1600,
+        alt: 'Um D grande seguindo de enuv em minusculo',
+      },
+    ],
+    description:
+      'Nosso compromisso é proporcionar resultados mensuráveis na internet. Ajudamos você a alcançar seus objetivos, aumentar sua visibilidade e, o mais importante, sua receita.' as string,
+    type: 'website',
+  },
+  icons: {
+    icon: 'favicon.ico',
+    shortcut: 'favicon.ico',
+    apple: 'favicon.ico',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Studio',
+    description:
+      'Nosso compromisso é proporcionar resultados mensuráveis na internet. Ajudamos você a alcançar seus objetivos, aumentar sua visibilidade e, o mais importante, sua receita.' as string,
+    images: 'favicon.ico',
   },
 }
 
